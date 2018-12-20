b707d6febd6d9ef30dc4081f681698009c548abf
hihi789456123 <45947453+hihi789456123@users.noreply.github.com>
2018-12-19 22:00:00 -0600

    ttt
    oh wow

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		promise.then(function() {}).catch(function(error) {
 			var autoplayCover = document.createElement("div");
 			autoplayCover.id = "autoplay-cover";
-			autoplayCover.innerHTML = `<div class="space" ></div><div class="space"></div><img src="img/tmus.png"><p style="color:#f0fff0;">Operations Interrupted. Click to Continue...</p>`;
+			autoplayCover.innerHTML = `<div class="space"></div><div class="space"></div><img src="img/tmus.png"><p style="color:#f0fff0;">Operations Interrupted. Click to Continue...</p>`;
 			document.body.appendChild(autoplayCover);
 			autoplayCover.addEventListener("click", function() {
 				autoplayCover.className = "hide";
