b637d0b1872c96baae938088fb12c2e0722a54a3
hihi789456123 <45947453+hihi789456123@users.noreply.github.com>
2018-12-19 21:45:18 -0600

    rivalry sound working
    also fixed some css

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var promise = document.getElementById("autoplay").play();
+
+	if (promise !== undefined) {
+		promise.then(function() {}).catch(function(error) {
+			var autoplayCover = document.createElement("div");
+			autoplayCover.id = "autoplay-cover";
+			autoplayCover.innerHTML = `<div class="space"></div><div class="space"></div><img src="img/tmus.png"><p style="color:#f0fff0;">Operations Interrupted. Click to Continue...</p>`;
+			document.body.appendChild(autoplayCover);
+			autoplayCover.addEventListener("click", function() {
+				autoplayCover.className = "hide";
+				setTimeout(function() {
+					document.body.removeChild(autoplayCover);
+				}, 1000);
+				document.getElementById("autoplay").play();
+			});
+		});
+	}
+});
+
